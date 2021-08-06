1a2b77b8e4c3e81f495151d12cca6e82890eb372
Fugax08 <67953267+Fugax08@users.noreply.github.com>
2021-08-06 10:28:19 +0200

    added entity Utente

---
+++ src/main/java/cost/management/entities/Utente.java
@@ -0,0 +1,40 @@
+package cost.management.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the utente database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Utente.findAll", query="SELECT u FROM Utente u")
+public class Utente implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String email;
+
+	private String password;
+
+	public Utente() {
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+}
