78c9e7189e2a17860f8c3db30b779cb619fb6bdc
Guidus Vendra <guido.vndr@gmail.com>
2021-08-25 10:30:53 +0200

    adjusting controller ?

--- src/main/java/cost/management/controller/DipendenteController.java
+++ src/main/java/cost/management/controller/DipendenteController.java
@@ -27,66 +27,63 @@ import cost.management.service.DipendenteService;
 //to cross origin to a REACT project use "http://localhost:3000"
 @CrossOrigin(origins = "http://localhost:4200")
 public class DipendenteController {
-	
+
 	@Autowired
 	private DipendenteService dipService;
-	
-	//inserici un dipendente nel db
+
+	// inserici un dipendente nel db
 	@PostMapping("/dipendenti")
 	public Dipendente addDipendente(@Valid @RequestBody Dipendente dipendente) {
 		/*
-		Dipendente newDip = new Dipendente();
-		newDip.setCodiceFiscale(dipendente.getCodiceFiscale());
-		newDip.setNome(dipendente.getNome());
-		newDip.setCognome(dipendente.getCognome());
-		newDip.setCellulare(dipendente.getCellulare());
-		newDip.setDataNascita(dipendente.getDataNascita());
-		newDip.setEmail(dipendente.getEmail());
-		newDip.setResidenza(dipendente.getResidenza());
-		newDip.setLuogoNascita(dipendente.getLuogoNascita());
-		newDip.setAzienda(dipendente.getAzienda());
-		
-		System.out.println("########### " +newDip +" ################");
-		*/
+		 * Dipendente newDip = new Dipendente();
+		 * newDip.setCodiceFiscale(dipendente.getCodiceFiscale());
+		 * newDip.setNome(dipendente.getNome());
+		 * newDip.setCognome(dipendente.getCognome());
+		 * newDip.setCellulare(dipendente.getCellulare());
+		 * newDip.setDataNascita(dipendente.getDataNascita());
+		 * newDip.setEmail(dipendente.getEmail());
+		 * newDip.setResidenza(dipendente.getResidenza());
+		 * newDip.setLuogoNascita(dipendente.getLuogoNascita());
+		 * newDip.setAzienda(dipendente.getAzienda());
+		 * 
+		 * System.out.println("########### " +newDip +" ################");
+		 */
 		return dipService.addDipendente(dipendente);
-		
+
 	}
-	
-	//trova tutti i dipendenti
+
+	// trova tutti i dipendenti
 	@GetMapping("/dipendenti")
-	public List<Dipendente> findAllDipendenti(){
-		
+	public List<Dipendente> findAllDipendenti() {
+
 		List<Dipendente> listaDipendenti = new ArrayList<Dipendente>();
 		listaDipendenti = dipService.findAllDipendenti();
 		return listaDipendenti;
 	}
-	
-	//find dipendente by codice fiscale
+
+	// find dipendente by codice fiscale
 	@GetMapping("/dipendenti/{codiceFiscale}")
 	public Optional<Dipendente> findDipendenteByCodiceFiscale(@PathVariable String codiceFiscale) {
-		
+
 		return dipService.findDipendenteByCodiceFiscale(codiceFiscale);
 	}
-	
-	//update un dipendente
+
+	// update un dipendente
 	@PutMapping("/dipendenti/{codiceFiscale}")
-	public Dipendente updateDipendente(@RequestBody Dipendente dipendente,
-			@PathVariable  String codiceFiscale) {
-		
+	public Dipendente updateDipendente(@RequestBody Dipendente dipendente, @PathVariable String codiceFiscale) {
+
 		return dipService.updateDipendente(dipendente, codiceFiscale);
 	}
-	
-	
-	//delete un dipendente
+
+	// delete un dipendente
 	@DeleteMapping("/dipendenti/{codiceFiscale}")
 	public Map<String, Boolean> deleteDipendente(@PathVariable String codiceFiscale) {
-		
+
 		Map<String, Boolean> deletion = new HashMap<>();
-			
+
 		dipService.deleteDipendente(codiceFiscale);
 		deletion.put("Dipendente Deleted", Boolean.TRUE);
 		return deletion;
 	}
-	
-	
+
 }
